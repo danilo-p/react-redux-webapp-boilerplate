6b26835514cfa050b3ba096f520028207e7b2d09
Danilo Pimentel <danilo.pimentel97@gmail.com>
2017-10-31 23:10:35 -0200

    FEA : Added support to tests with Jest.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  setupFiles: ["./node_modules/raf/polyfill.js"],
+  setupTestFrameworkScriptFile: "./node_modules/jest-enzyme/lib/index.js",
+  moduleNameMapper: {
+    ".*\\.(css|styl|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+/**
+ * TODO
+ * - [x] Add tests with Jest
+ * - [] Study Jest
+ * - [] Study Enzyme
+ * - [] Add support to raw files
+ * - [] Check if the images are being placed at dist/assets
+ * - [] Add support for PugJs on the index.html template
+ * - [] Configure build for production
+ * - [] Add support to project configuration (maybe common_config?)
+ * - [] Search and add references to the used JS guidelines
+ * - [] Search and add references to the used React guidelines
+ * - [] Search and add references to the used styles guidelines
+ * - [] Add React Router
+ * - [] Add Redux
+ * - [] Add RxJS
+ * - [] Add GraphQL
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
